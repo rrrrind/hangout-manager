dcdf4b480886ca2ebddabe5455039f59a6a17f29
rrrrind <masaya.mori0905@gmail.com>
2020-08-16 10:47:30 +0900

    Comp the friends recomentation model

--- workspace/release/app.py
+++ workspace/release/app.py
@@ -4,6 +4,7 @@ import json
 import urllib3
 
 from main import RecommendHangout
+from main import RecommendFriends
 
 app = Flask(__name__)
 http = urllib3.PoolManager()
@@ -12,6 +13,16 @@ http = urllib3.PoolManager()
 def get_hangout(hang_out_id):
     url = 'http://web:3000/hang_outs/' + hang_out_id + '/json'
     res = http.request('GET',url)
+    return res.data.decode('utf-8')
+
+@app.route("/current/<user_id>")
+def get_current_user(user_id):
+    url = 'http://web:3000/to_current/' + user_id + '/json'
+    res = http.request('GET',url)
+    data = json.loads(res.data.decode('utf-8'))
+    current_list = [data['p_one'], data['p_two'], data['p_three'], data['p_four'], data['user_id']]
+    print (data)
+    print (current_list)
     return res.data.decode('utf-8')
 
 @app.route("/questions/<question_id>")
@@ -30,7 +41,7 @@ def get_question(question_id):
     d6 = data['user_detail']['d_six']
     d7 = data['user_detail']['d_seven']
     d8 = data['user_detail']['d_eight']
-
+    print (data)
     rh = RecommendHangout([d1,d2,d3,d4,d5,d6,d7,d8],[q1,q2,q3])
     genre, hangouts = rh.forward()
     return_rec = dict(a1=hangouts[0],
@@ -67,12 +78,63 @@ def get_user_personal(user_personal_id):
     p2 = user_personal_json['p_two']
     p3 = user_personal_json['p_three']
     p4 = user_personal_json['p_four']
-    p5 = user_personal_json['p_five']
-    p6 = user_personal_json['p_six']
-    print ('p1', p1, 'p2', p2, 'p3', p3, 'p4', p4, 'p5', p5, 'p6', p6)
+    print ('p1', p1, 'p2', p2, 'p3', p3, 'p4', p4)
     return res.data.decode('utf-8')
+
+@app.route("/users")
+def get_users():
+    url = 'http://web:3000/user_all/json'
+    res = http.request('GET',url)
+    users_json = json.loads(res.data.decode('utf-8'))
+    user_list = []
+    user_personal_list = []
+    for i in range(len(users_json['user_personals'])):
+        user_personal_list += [users_json['user_personals'][i]['p_one']]
+        user_personal_list += [users_json['user_personals'][i]['p_two']]
+        user_personal_list += [users_json['user_personals'][i]['p_three']]
+        user_personal_list += [users_json['user_personals'][i]['p_four']]
+        user_personal_list += [users_json['user_personals'][i]['user_id']]
+        user_list += [user_personal_list]
+        user_personal_list = []
+
+    print (user_list) # user_list->Userの情報[p_one, p_two, p_three, p_four, user_id]
+    return res.data.decode('utf-8')
+
+@app.route("/friend/<current_user_id>")
+def get_friend(current_user_id):
+    url_all = 'http://web:3000/user_all/json'
+    res_all = http.request('GET',url_all)
+    users_json = json.loads(res_all.data.decode('utf-8'))
+    user_list = []
+    user_personal_list = []
+    for i in range(len(users_json['user_personals'])):
+        user_personal_list += [users_json['user_personals'][i]['p_one']]
+        user_personal_list += [users_json['user_personals'][i]['p_two']]
+        user_personal_list += [users_json['user_personals'][i]['p_three']]
+        user_personal_list += [users_json['user_personals'][i]['p_four']]
+        user_personal_list += [users_json['user_personals'][i]['user_id']]
+        user_list += [user_personal_list]
+        user_personal_list = []
+
+    url_current = 'http://web:3000/to_current/' + current_user_id + '/json'
+    res_current = http.request('GET',url_current)
+    current_json = json.loads(res_current.data.decode('utf-8'))
+    current_list = [current_json['p_one'], current_json['p_two'], current_json['p_three'], current_json['p_four'], current_json['user_id']]
+    
+    rf = RecommendFriends(user_list,current_list)
+    rf_rank = rf.forward()   
+    return_rank = dict(ID0=int(rf_rank[0,1]),
+                       ID1=int(rf_rank[1,1]),
+                       ID2=int(rf_rank[2,1]),
+                       ID3=int(rf_rank[3,1]),
+                       ID4=int(rf_rank[4,1]),
+                       ID5=int(rf_rank[5,1]),
+                       ID6=int(rf_rank[6,1]),
+                       ID7=int(rf_rank[7,1]),
+                       ID8=int(rf_rank[8,1]),
+                       ID9=int(rf_rank[9,1]))    
+    return return_rank
 
 ## おまじない
 if __name__ == "__main__":
     app.run(debug=True)
-
